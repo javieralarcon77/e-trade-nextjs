16a61cf47380477b7d456ce9ecb9f3eb3fa0d09e
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2026-01-30 19:10:14 -0500

    suppress warning hidratation

--- components/organisms/testimonials/testimonials.tsx
+++ components/organisms/testimonials/testimonials.tsx
@@ -24,7 +24,7 @@ export function Testimonials() {
   }
 
   return (
-    <section className="testimonials">
+    <section suppressHydrationWarning={true} className="testimonials">
       <HeaderSection
         title="Testimonials"
         subtitle="Users Feedback"
