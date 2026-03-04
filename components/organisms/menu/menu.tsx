1175824ab7188cb3afc6214e58eb83028f445c62
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2026-03-04 18:12:35 -0500

    agregado menu superior

---
+++ components/organisms/menu/menu.tsx
@@ -0,0 +1,41 @@
+import "./menu.css";
+
+export function Menu() {
+  return (
+    <div className="menu-container">
+      <menu>
+        <div>
+          <img src="/logo.png" className="logo" height="35px" />
+        </div>
+        <nav>
+          <li>
+            <a href="#">
+              <img src="/icons/search.png" width={20} />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="/icons/heart.png" width={20} />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="car-count">3</span>
+              <img src="/icons/car.png" width={20} />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="/icons/user.png" width={20} />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="/icons/menu.png" width={20} />
+            </a>
+          </li>
+        </nav>
+      </menu>
+    </div>
+  );
+}
