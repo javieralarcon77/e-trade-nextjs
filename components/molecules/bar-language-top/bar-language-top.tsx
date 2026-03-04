0919de1ee40bcc2441c46751e93cc3484428f9c8
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2026-03-04 18:10:52 -0500

    agregada barra de lenguaje

---
+++ components/molecules/bar-language-top/bar-language-top.tsx
@@ -0,0 +1,17 @@
+import "./bar-language-top.css";
+
+export function BarLanguageTop() {
+  return (
+    <div className="bar-language-top">
+      <div>
+        <button>English</button>
+        <button>USD</button>
+      </div>
+      <div>
+        <a href="#">Help</a>
+        <a href="#">Join Us</a>
+        <a href="#">Sign In</a>
+      </div>
+    </div>
+  );
+}
