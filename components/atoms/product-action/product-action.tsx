e2aae84b4798cc9de7775ceb519af5db7b9a8ba4
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2026-05-08 19:12:20 -0500

    ajustados estilos para product actions

--- components/atoms/product-action/product-action.tsx
+++ components/atoms/product-action/product-action.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import "./product-action.css";
 
 export function ProductAction({ product }: any) {
+  const [count, setCount] = useState<number>(0);
+
+  function plusCount() {
+    setCount(count + 1);
+  }
+
+  function minusCount() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="product-action">
       <div className="product-action-counter">
-        <button>-</button>
-        <span>1</span>
-        <button>+</button>
+        <button onClick={minusCount}>-</button>
+        <span>{count}</span>
+        <button onClick={plusCount}>+</button>
       </div>
       <button className="product-action-add-to-cart">Add to Cart</button>
       <button className="product-action-favorite">
